4e6343192af4613dd9e504e3bb232814e8266433
sunshinePlusIce <sunshineplusice@gmail.com>
2023-04-11 00:25:06 +0800

    do some code reuse for UserResource

--- src/main/java/com/arato/Mezashi/User/UserResource.java
+++ src/main/java/com/arato/Mezashi/User/UserResource.java
@@ -1,7 +1,12 @@
 package com.arato.Mezashi.User;
 
+import com.arato.Mezashi.User.exception.UserNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class UserResource {
@@ -12,12 +17,19 @@ public class UserResource {
 
     @GetMapping("/users/{userId}")
     public User getUser(@PathVariable long userId) {
-
-        return new User("arato", "password");
+        return _findUserById(userId);
     }
 
     @PostMapping("/users")
-    public void createUser(@Valid @RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         this.userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    private User _findUserById(long userId) {
+        var user = this.userRepository.findById(userId);
+        if (user.isEmpty()) throw new UserNotFoundException("user with id=" + userId + " is not found");
+        return user.get();
     }
 }
